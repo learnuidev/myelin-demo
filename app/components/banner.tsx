b98af34c76ec83cef1d4f1990b5ab59346fa3419
learnuidev <learnuidev@gmail.com>
2025-02-04 16:10:49 -0500

    added cta translation

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useTranslation } from "@/libs/i18n-next/use-translation";
 
 export const Banner = () => {
-  const { t } = useTranslation("banner");
+  const { t } = useTranslation(["banner", "common"]);
   return (
     <section className="flex flex-col justify-center items-center mt-32">
       <h1 className="text-5xl font-bold">{t("title")}</h1>
 
       <p className="mt-4 text-xl font-mono">{t("description")}</p>
+
+      <Button className="rounded-full mt-4">{t("cta")}</Button>
     </section>
   );
 };
